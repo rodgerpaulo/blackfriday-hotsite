bafa0c34e52b0f3ce57066ccc9276c1af6ebb8e3
Roger Ramos <rodgerpaulo@hotmail.com>
2018-07-17 23:40:02 -0300

    removing shopping cart from components index

--- src/javascripts/components/components.js
+++ src/javascripts/components/components.js
@@ -1,6 +1,5 @@
 export * from './navbar/navbar';
 export * from './banner/banner';
-export * from './shopping-cart/shopping-cart';
 export * from './product/product';
 export * from './icons/icons';
 export * from './section-title/section-title';
