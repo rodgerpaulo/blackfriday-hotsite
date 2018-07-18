2b423e4ea431df2479e2879dd11542adf12935f4
Roger Ramos <rodgerpaulo@hotmail.com>
2018-07-17 23:39:29 -0300

    adding redux logger

--- src/javascripts/stores/index.js
+++ src/javascripts/stores/index.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from 'javascripts/reducers/';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(rootReducer);
+const loggerMiddleware = createLogger({
+  collapsed: true
+});
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(loggerMiddleware)
+);
+/* eslint-enable */
 
 export default store;
