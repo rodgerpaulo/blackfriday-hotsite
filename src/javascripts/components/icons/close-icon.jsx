f29d7858ac43f111493a5e71be31dcba9099e627
Roger Ramos <rodgerpaulo@hotmail.com>
2018-07-18 01:29:28 -0300

    adding close icon

---
+++ src/javascripts/components/icons/close-icon.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import BaseSvg from 'javascripts/components/icons/base-svg';
+import { string } from 'prop-types';
+
+export class CloseIcon extends Component {
+  render() {
+    const { width, height, className, color } = this.props;
+    return (
+      <BaseSvg
+        className={className}
+        width={width}
+        height={height}
+        color={color}
+      >
+        <g fill="none" fillRule="evenodd" opacity=".54">
+          <g transform="translate(-415 -99)" fill={color}>
+            <g transform="translate(416 99)">
+              <polygon points="19 6.4 17.6 5 12 10.6 6.4 5 5 6.4 10.6 12 5 17.6 6.4 19 12 13.4 17.6 19 19 17.6 13.4 12" />
+            </g>
+          </g>
+        </g>
+      </BaseSvg>
+    );
+  }
+}
+
+CloseIcon.propTypes = {
+  className: string,
+  width: string,
+  height: string,
+  color: string
+};
+
+CloseIcon.defaultProps = {
+  className: '',
+  width: '24',
+  height: '24',
+  color: '#505050'
+};
--- src/javascripts/components/icons/icons.js
+++ src/javascripts/components/icons/icons.js
@@ -2,3 +2,4 @@ export * from './base-svg';
 export * from './shopping-cart-icon';
 export * from './percent-badge-icon';
 export * from './discount-badge-icon';
+export * from './close-icon';
