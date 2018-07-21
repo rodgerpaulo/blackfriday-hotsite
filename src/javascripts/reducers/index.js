7b90e64b10bc6af798bbbce71f647f743478e2f4
Roger Ramos <rodgerpaulo@hotmail.com>
2018-07-21 11:59:38 -0300

    adding lodash and data persistence

--- src/javascripts/reducers/index.js
+++ src/javascripts/reducers/index.js
@@ -1,8 +1,11 @@
-import { ADD_TO_CART } from 'javascripts/constants/action-types';
-import { UPDATE_CART_ITEM } from 'javascripts/constants/action-types';
-import { REMOVE_CART_ITEM } from 'javascripts/constants/action-types';
+import { loadState } from 'javascripts/helpers/localStorage';
+import {
+  ADD_TO_CART,
+  UPDATE_CART_ITEM,
+  REMOVE_CART_ITEM
+} from 'javascripts/constants/action-types';
 
-const initialState = {
+let initialState = loadState() || {
   byId: [],
   byHash: {}
 };
@@ -11,7 +14,7 @@ const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return {
-        byId: [ ...state.byId, action.id ],
+        byId: [...state.byId, action.id],
         byHash: {
           ...state.byHash,
           [action.id]: action.payload
